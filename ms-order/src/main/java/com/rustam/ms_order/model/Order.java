65598eb368c52162cb371bc62ae16abe7f6fe0c2
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-17 21:43:52 +0400

    Create Order.java

---
+++ ms-order/src/main/java/com/rustam/ms_order/model/Order.java
@@ -0,0 +1,26 @@
+package com.rustam.ms_order.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long customerId;
+
+    private String orderName;
+
+    private Long orderNumber;
+}

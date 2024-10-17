2f32d00392588665a3637cac03966eaa86e706ca
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-17 21:43:36 +0400

    Create OrderDto.java

---
+++ ms-order/src/main/java/com/rustam/ms_order/dto/OrderDto.java
@@ -0,0 +1,18 @@
+package com.rustam.ms_order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderDto {
+
+    private Long orderNumber;
+
+    private String orderName;
+
+}

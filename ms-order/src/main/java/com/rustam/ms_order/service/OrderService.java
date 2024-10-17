f175a51183e60c45781d1cd0ca3a5f4903e46926
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-17 21:44:00 +0400

    Create OrderService.java

---
+++ ms-order/src/main/java/com/rustam/ms_order/service/OrderService.java
@@ -0,0 +1,30 @@
+package com.rustam.ms_order.service;
+
+import com.rustam.ms_order.dto.OrderDto;
+import com.rustam.ms_order.mapper.OrderMapper;
+import com.rustam.ms_order.model.Order;
+import com.rustam.ms_order.repository.OrderRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true,level = AccessLevel.PRIVATE)
+public class OrderService {
+
+    OrderRepository orderRepository;
+
+    public OrderDto save(OrderDto orderDto) {
+        Order order = Order.builder()
+                .orderName(orderDto.getOrderName())
+                .orderNumber(orderDto.getOrderNumber())
+                .build();
+        orderRepository.save(order);
+        return OrderMapper.ORDER_MAPPER.convert(order);
+    }
+
+    public OrderDto read() {
+    }
+}

7177ea749045410c22e705a3f5f8d48c42a9612a
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-18 14:27:14 +0400

    Create Customer.java

---
+++ ms-customer/src/main/java/com/rustam/ms_customer/model/Customer.java
@@ -0,0 +1,31 @@
+package com.rustam.ms_customer.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    private String accountNumber;
+
+    private String userId;
+
+    private String username;
+}

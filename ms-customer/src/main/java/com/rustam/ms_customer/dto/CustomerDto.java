e5610f4966826e5f96a9e5d7d75efbd62245f258
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-18 14:27:01 +0400

    Create CustomerDto.java

---
+++ ms-customer/src/main/java/com/rustam/ms_customer/dto/CustomerDto.java
@@ -0,0 +1,18 @@
+package com.rustam.ms_customer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDto {
+
+    private String name;
+
+    private String surname;
+
+}

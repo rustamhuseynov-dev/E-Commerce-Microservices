177ab69959e86be9e12162cd234b89dca7f9d729
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-19 12:53:25 +0400

    Create JwtAuthenticationFilter.java

---
+++ api-gateway/src/main/java/com/rustam/api_gateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,15 @@
+package com.rustam.api_gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthenticationFilter implements GatewayFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+}

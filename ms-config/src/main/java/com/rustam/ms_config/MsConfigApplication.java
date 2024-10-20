364f5be6e55edfafe90fe4a138f39a0c8076c914
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2024-10-20 18:09:21 +0400

    refresh config

---
+++ ms-config/src/main/java/com/rustam/ms_config/MsConfigApplication.java
@@ -0,0 +1,15 @@
+package com.rustam.ms_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConfigApplication.class, args);
+	}
+
+}
